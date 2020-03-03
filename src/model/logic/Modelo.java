77e898588732e001e6cdfd7b326138ab777e0f30
Cecilia <cecilia@192.168.1.129>
2020-03-02 23:18:00 -0500

    Tests

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -117,7 +117,81 @@ public class Modelo
 		try {
 
 			int mayorID  = 0;
-			reader = new JsonReader(new FileReader(PATH));
+			reader = new JsonReader(new FileReader( PATH));
+			JsonParser jsonp = new JsonParser();
+
+			JsonElement elem = jsonp.parse(reader);
+			JsonArray e2 = elem.getAsJsonObject().get("features").getAsJsonArray();
+
+
+			SimpleDateFormat parser=new SimpleDateFormat("yyyy/MM/dd");
+
+			for(JsonElement e: e2) {
+				int OBJECTID = e.getAsJsonObject().get("properties").getAsJsonObject().get("OBJECTID").getAsInt();
+
+				String s = e.getAsJsonObject().get("properties").getAsJsonObject().get("FECHA_HORA").getAsString();	
+				Date FECHA_HORA = parser.parse(s); 
+
+				String MEDIO_DETE = e.getAsJsonObject().get("properties").getAsJsonObject().get("MEDIO_DETE").getAsString();
+				String CLASE_VEHI = e.getAsJsonObject().get("properties").getAsJsonObject().get("CLASE_VEHI").getAsString();
+				String TIPO_SERVI = e.getAsJsonObject().get("properties").getAsJsonObject().get("TIPO_SERVI").getAsString();
+				String INFRACCION = e.getAsJsonObject().get("properties").getAsJsonObject().get("INFRACCION").getAsString();
+				String DES_INFRAC = e.getAsJsonObject().get("properties").getAsJsonObject().get("DES_INFRAC").getAsString();	
+				String LOCALIDAD = e.getAsJsonObject().get("properties").getAsJsonObject().get("LOCALIDAD").getAsString();
+
+				double longitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
+						.get(0).getAsDouble();
+
+				double latitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
+						.get(1).getAsDouble();
+
+				Comparendo c = new Comparendo(OBJECTID, FECHA_HORA, MEDIO_DETE, CLASE_VEHI, TIPO_SERVI, INFRACCION,DES_INFRAC, LOCALIDAD, longitud, latitud);
+				datos.enqueue(c);
+
+				if(latitud < menorLatitud)
+				{
+					menorLatitud = latitud;
+				}
+
+				if(latitud > mayorLatitud)
+				{
+					mayorLatitud = latitud;
+				}
+
+				if(longitud < menorLongitud)
+				{
+					menorLongitud = longitud;
+				}
+
+				if(longitud > mayorLongitud)
+				{
+					mayorLongitud = longitud;
+				}
+
+				if(OBJECTID > mayorID)
+				{
+					mayorComparendo = c;
+				}
+			}
+
+		} catch (FileNotFoundException | ParseException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
+
+	}
+	
+	
+	public void cargarDatosSmall() 
+
+	{
+		JsonReader reader;
+
+		try {
+
+			int mayorID  = 0;
+			reader = new JsonReader(new FileReader("./data/comparendos_dei_2018_small.geojson"));
 			JsonParser jsonp = new JsonParser();
 
 			JsonElement elem = jsonp.parse(reader);
---
+++ test/test/logic/TestModelo.java
@@ -0,0 +1,185 @@
+package test.logic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.data_structures.*;
+import model.logic.*;
+
+public class TestModelo 
+{
+
+	private Modelo modelo; 
+
+
+	@Before
+	public void setUpEscenario1()
+	{
+		modelo = new Modelo();
+		modelo.cargarDatosSmall();
+	}
+
+
+	@Test
+	public void testModelo()
+	{
+		assertTrue(modelo != null);
+	}
+
+
+	@Test 
+	public void testDarTamanoCola()
+	{
+		assertEquals("El tamaño de la cola no es el esperado",20, modelo.darTamanoCola());
+	}
+
+	@Test
+	public void testDarPrimeroCola()
+	{
+		assertEquals("El primer comparendo no es el esperado", 29042, modelo.darPrimeroCola().darObjectID());
+	}
+
+	@Test
+	public void testDarUltimoCola()
+	{
+		assertEquals("El ultimo comparendo no es el esperado", 209146, modelo.darUltimoCola().darObjectID());
+	}
+
+
+	//Test B1
+	@Test
+	public void testDarPrimerComparendoPorInfraccion()
+	{
+		Comparendo buscado = modelo.darPrimerComparendoPorInfraccion("C02");
+		assertEquals("El comparendo encontrado no es el esperado", 29042, buscado.darObjectID() );
+
+
+		buscado  = modelo.darPrimerComparendoPorInfraccion("C35");
+		assertEquals("El comparendo encontrado no es el esperado", 509329, buscado.darObjectID());
+
+	} 
+
+
+	//Test B2
+	public void testDarComparendosPorInfraccion()
+	{
+		Comparable[] testear = modelo.darComparendosPorInfraccion("C02");
+
+		assertEquals("El numero de comparendos encontrados no es el esperado", 9, testear.length);
+
+		for(int i =0; i< testear.length; i++ )
+		{
+			Comparendo actual = (Comparendo) testear[i];
+			assertEquals("El comparendo no tiene la infraccion deseada", "C02", actual.darInfraccion());
+		}
+
+	}
+
+	//Test B3
+	public void testDarComparendosPorTipo()
+	{
+		ArrayList<String[]> testear =  modelo.darComparendosPorTipo();
+
+		//Chequear algunos codigos
+		assertEquals("El codigo de la infraccion no corresponde al esperado", "A08", testear.get(0)[0]);
+		assertEquals("El codigo de la infraccion no corresponde al esperado", "B02", testear.get(1)[0]);
+		assertEquals("El codigo de la infraccion no corresponde al esperado", "D02", testear.get(8)[0]);
+		assertEquals("El codigo de la infraccion no corresponde al esperado", "H03", testear.get(9)[0]);
+
+
+		//Chequear algunos datos
+
+		assertEquals("El numero de comparendos de tipo privado no es el esperado", 1, testear.get(0)[1]);
+		assertEquals("El numero de comparendos de tipo publico no es el esperado", 0, testear.get(0)[2]);
+
+		assertEquals("El numero de comparendos de tipo privado no es el esperado", 1, testear.get(1)[1]);
+		assertEquals("El numero de comparendos de tipo publico no es el esperado", 0, testear.get(1)[2]);
+
+		assertEquals("El numero de comparendos de tipo privado no es el esperado", 8, testear.get(2)[1]);
+		assertEquals("El numero de comparendos de tipo publico no es el esperado", 1, testear.get(2)[2]);
+
+		assertEquals("El numero de comparendos de tipo privado no es el esperado", 1, testear.get(8)[1]);
+		assertEquals("El numero de comparendos de tipo publico no es el esperado", 0, testear.get(8)[2]);
+
+		assertEquals("El numero de comparendos de tipo privado no es el esperado", 1, testear.get(9)[1]);
+		assertEquals("El numero de comparendos de tipo publico no es el esperado", 0, testear.get(9)[2]);
+
+
+
+
+	}
+
+
+	//Test A1
+	public void testNumeroComparendosPorLocalidadYFecha()
+	{
+		try 
+		{
+
+			SimpleDateFormat parser=new SimpleDateFormat("yyyy/MM/dd");
+			
+			Date fechaI= parser.parse("2018/01/01");
+			Date fechaF = parser.parse("2018/12/31");
+			
+			ArrayList<String[]> testear = modelo.numeroComparendosPorLocalidadYFecha("TEUSAQUILLO", fechaI, fechaF );
+			
+			assertEquals("El numero de comparendos en esta localidad y entre estas fechas no es el esperado", 3, testear.size());
+			
+			assertEquals("El codigo de la infraccion no es el esperado", "A08", testear.get(0)[0]);
+			assertEquals("El numero de comparendos por infraccion no es el esperado", 1, testear.get(0)[1]);
+			
+			assertEquals("El codigo de la infraccion no es el esperado", "C02", testear.get(1)[0]);
+			assertEquals("El numero de comparendos por infraccion no es el esperado", 1, testear.get(1)[1]);
+			
+			assertEquals("El codigo de la infraccion no es el esperado", "C31", testear.get(2)[0]);
+			assertEquals("El numero de comparendos por infraccion no es el esperado", 1, testear.get(2)[1]);
+		}
+
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	//Test A2
+	public void testDarNComparendosPorFecha()
+	{
+		try 
+		{
+
+			SimpleDateFormat parser=new SimpleDateFormat("yyyy/MM/dd");
+			
+			Date fechaI= parser.parse("2018/01/01");
+			Date fechaF = parser.parse("2018/12/31");
+			
+			ArrayList<String[]> testear = modelo.darNComparendosPorFecha(3, fechaI, fechaF);
+			
+			assertEquals("El numero de infracciones no es el deseado", 3, testear.size());
+			
+			assertEquals("El codigo de la infraccion no es el esperado", "C02", testear.get(0)[0]);
+			assertEquals("El numero de comparendos por infraccion no es el esperado", 9, testear.get(0)[1]);
+			
+			assertEquals("El codigo de la infraccion no es el esperado", "C14", testear.get(1)[0]);
+			assertEquals("El numero de comparendos por infraccion no es el esperado", 2, testear.get(1)[1]);
+			
+			
+			assertEquals("El codigo de la infraccion no es el esperado", "C32", testear.get(2)[0]);
+			assertEquals("El numero de comparendos por infraccion no es el esperado", 2, testear.get(2)[1]);
+			
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}
+}
